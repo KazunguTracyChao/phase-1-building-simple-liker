e3794c22db877bb4cd4462623163a75de7beb44a
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-03-29 11:36:50 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,31 +4,72 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-const articleHearts = document.querySelectorAll(".like-glyph");
+const modal = document.querySelector('#modal');
+const likeBtn = document.querySelector('.like-glyph');
 
-function likeCallback(e) {
-  const heart = e.target;
-  mimicServerCall("bogusUrl")
-    .then(function(){
-      if ( heart.innerText === EMPTY_HEART) {
-        heart.innerText = FULL_HEART;
-        heart.className = "activated-heart";
-      } else {
-        heart.innerText = EMPTY_HEART;
-        heart.className = "";
+modal.classList.add('hidden');  // Add .hidden class to error modal in HTML
+
+likeBtn.addEventListener('click', (e) => {  // When the like button is clicked
+  mimicServerCall()  // simulate making a server request
+    .then(response => {
+      if (response === "success") {  // when the "server" returns a success status
+        e.target.innerText = '❤️';  // update the heart to be full
+        e.target.classList.add('activated-heart');  // add the class to make the heart red
+      } else {  // when the "server" returns a failure status
+        modal.classList.remove('hidden');  // display the error modal by removing .hidden class
+        const errorMessage = document.querySelector('#modal p');
+        errorMessage.innerText = "Oops! Something went wrong!";  // display the server error message
+        setTimeout(() => modal.classList.add('hidden'), 3000);  // hide the modal after 3 seconds
       }
     })
-    .catch(function(error) {
-      const modal = document.getElementById("modal");
-      modal.className = "";
-      modal.innerText = error;
-      setTimeout(() =>  modal.className = "hidden", 3000);
+    .catch(error => {
+      modal.classList.remove('hidden');  // display the error modal by removing .hidden class
+      const errorMessage = document.querySelector('#modal p');
+      errorMessage.innerText = "Oops! Something went wrong!";  // display the server error message
+      setTimeout(() => modal.classList.add('hidden'), 3000);  // hide the modal after 3 seconds
     });
-}
+});
 
-for (const glyph of articleHearts) {
-  glyph.addEventListener("click", likeCallback);
-}
+likeBtn.addEventListener('dblclick', (e) => {  // When the full heart is clicked
+  e.target.innerText = '♡';  // set the heart back to empty
+  e.target.classList.remove('activated-heart');  // remove the class to make the heart red
+});
+
+
+
+
+/// MORE ADVANCED SOLUTION ///
+
+// const glyphStates = {
+//   "♡": "♥",
+//   "♥": "♡"
+// };
+
+// const colorStates = {
+//   "red" : "",
+//   "": "red"
+// };
+
+// const articleHearts = document.querySelectorAll(".like-glyph");
+
+// function likeCallback(e) {
+//   const heart = e.target;
+//   mimicServerCall("bogusUrl", {forceFailure: true})
+//     .then(function(){
+//        heart.innerText = glyphStates[heart.innerText];
+//        heart.style.color = colorStates[heart.style.color];
+//     })
+//     .catch(function(error) {
+//       const modal = document.getElementById("modal");
+//       modal.className = "";
+//       modal.innerText = error;
+//       setTimeout(() =>  modal.className = "hidden", 3000);
+//     });
+// }
+
+// for (const glyph of articleHearts) {
+//   glyph.addEventListener("click", likeCallback);
+// }
 
 
 
